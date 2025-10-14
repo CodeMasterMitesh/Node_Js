3eaa5664b00a2d0da40b881e4fe7887067bd5118
CodeMasterMitesh <prajapati.mitesh461@gmail.com>
2025-10-14 09:05:27 +0530

    update

--- lec_14_crud/server.js
+++ lec_14_crud/server.js
@@ -18,16 +18,66 @@ const server = createServer(async (req, res) => {
     if (req.method === 'GET') {
         if (req.url === '/') {
             const data = await readFile(indexPage, 'utf-8');
-            console.log(data);
+            // console.log(data);
             res.writeHead(200, { 'content-type': 'text/html' });
             res.end(data);
             // console.log(req.url);
+        }else if (req.url === '/style.css') {
+            const css = await readFile(path.join(__dirname,"public","style.css"), 'utf-8');
+            // console.log(data);
+            res.writeHead(200, { 'content-type': 'text/css' });
+            res.end(css);
+        }else if (req.url === '/login') {
+            const data = await readFile(path.join(__dirname,"public","login.html"), 'utf-8');
+            // console.log(data);
+            res.writeHead(200, { 'content-type': 'text/html' });
+            res.end(data);
         }
-    } else if (req.method === 'GET' && req.url === 'style.css') {
-        const data = await readFile(path.join(__dirname,"public","style.css"), 'utf-8');
-        console.log(data);
-        res.writeHead(200, { 'content-type': 'text/css' });
-        res.end(data);
+    } 
+
+    if(req.method === "POST" && req.url === '/studentRegData'){
+        let body = '';
+        req.on('data',(chunk)=>{
+            body+=chunk;
+            console.log(body);
+            const adata = new URLSearchParams(body);
+            const urlData = Object.fromEntries(adata);
+            // console.log(urlData);
+            res.writeHead(200, { 'content-type': 'text/html' });
+            
+            res.write(`<table border='1'>
+                <tr>
+                    <th colspan='2'>Student Data</th>
+                </tr>
+                <tr>
+                    <th>First Name:<th>
+                    <th>${urlData.firstname}</th>
+                </tr>
+                <tr>
+                    <th>Last Name:<th>
+                    <th>${urlData.lastname}</th>
+                </tr>
+                <tr>
+                    <th>Email:<th>
+                    <th>${urlData.email}</th>
+                </tr>
+                <tr>
+                    <th>Address:<th>
+                    <th>${urlData.address}</th>
+                </tr>
+                <tr>
+                    <th>Mobile:<th>
+                    <th>${urlData.mobile}</th>
+                </tr>
+                <tr>
+                    <th>Education:<th>
+                    <th>${urlData.education}</th>
+                </tr>
+                
+                `);
+            res.end();
+        });
+       
     }
 
 });
