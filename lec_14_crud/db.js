e35ed9c9c197aaf2e9413b3f6a5562153cea28ed
CodeMasterMitesh <prajapati.mitesh461@gmail.com>
2025-10-18 09:06:40 +0530

    update

--- lec_14_crud/db.js
+++ lec_14_crud/db.js
@@ -24,6 +24,12 @@ db.createCollection("users",
                         maximum: 100,
                         description: "Age must be 18-100"
                     },
+                    mobile: {
+                        bsonType: "int",
+                        minimum: 1,
+                        maximum: 10,
+                        description: "Mobile Number must 10 Digit"
+                    },
                     isActive: {
                         bsonType: "bool",
                         description: "Boolean field"
